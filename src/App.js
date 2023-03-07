e37da7d3c729809bcaaf9f47f4f7653b87c21920
abunbux <abunbux@gmail.com>
2023-03-07 22:42:09 +0300

    Первый коммит.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,181 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/main.css";
+
+import sun from "./img/icons/sun.svg";
+import moon from "./img/icons/moon.svg";
+
+import vk from "./img/icons/vk.svg";
+import instagram from "./img/icons/instagram.svg";
+import twitter from "./img/icons/twitter.svg";
+import gitHub from "./img/icons/gitHub.svg";
+import linkedIn from "./img/icons/linkedIn.svg";
+
+import project_01 from "./img/projects/project_01.jpg";
+import project_02 from "./img/projects/project_02.jpg";
+import project_03 from "./img/projects/project_03.jpg";
+import project_04 from "./img/projects/project_04.jpg";
+import project_05 from "./img/projects/project_05.jpg";
+import project_06 from "./img/projects/project_06.jpg";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <nav className="nav">
+        <div className="container">
+          <div className="nav-row">
+            <a href="./index.html" className="logo">
+              <strong>Frilancer</strong> portfolio
+            </a>
+
+            <button className="dark-mode-btn">
+              <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
+              <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />
+            </button>
+
+            <ul className="nav-list">
+              <li className="nav-list__item">
+                <a
+                  href="./index.html"
+                  className="nav-list__link nav-list__link--active">
+                  Projects
+                </a>
+              </li>
+              <li className="nav-list__item">
+                <a href="./skills.html" className="nav-list__link">
+                  Skills
+                </a>
+              </li>
+              <li className="nav-list__item">
+                <a href="./contacts.html" className="nav-list__link">
+                  Contacts
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <header className="header">
+        <div className="header__wrapper">
+          <h1 className="header__title">
+            <strong>
+              Hi, my name is <em>Pavel</em>
+            </strong>
+            <br />a frontend developer
+          </h1>
+          <div className="header__text">
+            <p>with passion for learning and creating.</p>
+          </div>
+          <a href="#!" className="btn">
+            Download CV
+          </a>
+        </div>
       </header>
+
+      <main className="section">
+        <div className="container">
+          <h2 className="title-1">Projects</h2>
+          <ul className="projects">
+            <li className="project">
+              <a href="./project-page.html">
+                <img
+                  src={project_01}
+                  alt="project img"
+                  className="project__img"
+                />
+                <h3 className="project__title">Gaming streaming portal</h3>
+              </a>
+            </li>
+            <li className="project">
+              <a href="./project-page.html">
+                <img
+                  src={project_02}
+                  alt="project img"
+                  className="project__img"
+                />
+                <h3 className="project__title">Video service</h3>
+              </a>
+            </li>
+            <li className="project">
+              <a href="./project-page.html">
+                <img
+                  src={project_03}
+                  alt="project img"
+                  className="project__img"
+                />
+                <h3 className="project__title">Video portal</h3>
+              </a>
+            </li>
+            <li className="project">
+              <a href="./project-page.html">
+                <img
+                  src={project_04}
+                  alt="project img"
+                  className="project__img"
+                />
+                <h3 className="project__title">Dating app</h3>
+              </a>
+            </li>
+            <li className="project">
+              <a href="./project-page.html">
+                <img
+                  src={project_05}
+                  alt="project img"
+                  className="project__img"
+                />
+                <h3 className="project__title">Landing</h3>
+              </a>
+            </li>
+            <li className="project">
+              <a href="./project-page.html">
+                <img
+                  src={project_06}
+                  alt="project img"
+                  className="project__img"
+                />
+                <h3 className="project__title">Gaming community</h3>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </main>
+
+      <footer className="footer">
+        <div className="container">
+          <div className="footer__wrapper">
+            <ul className="social">
+              <li className="social__item">
+                <a href="#!">
+                  <img src={vk} alt="Link" />
+                </a>
+              </li>
+              <li className="social__item">
+                <a href="#!">
+                  <img src={instagram} alt="Link" />
+                </a>
+              </li>
+              <li className="social__item">
+                <a href="#!">
+                  <img src={twitter} alt="Link" />
+                </a>
+              </li>
+              <li className="social__item">
+                <a href="#!">
+                  <img src={gitHub} alt="Link" />
+                </a>
+              </li>
+              <li className="social__item">
+                <a href="#!">
+                  <img src={linkedIn} alt="Link" />
+                </a>
+              </li>
+            </ul>
+            <div className="copyright">
+              <p>© 2022 frontend-dev.com</p>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }

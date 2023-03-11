ba9d2d0b439a669e5a7fd0916dc1cae241cee80a
abunbux <abunbux@gmail.com>
2023-03-11 20:47:02 +0300

    Сохранение темы при повторном посещении сайта.

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -1,18 +1,20 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
+import { useLocalStorage } from "./../../utils/useLocalStorage";
 import sun from "./../../img/icons/sun.svg";
 import moon from "./../../img/icons/moon.svg";
 
 const BtnDarkMode = () => {
-  const [darkMode, setDarkMode] = useState("light");
+  const [darkMode, setDarkMode] = useLocalStorage("darkMode", "light");
+
   const btnRef = useRef(null);
 
   useEffect(() => {
     if (darkMode === "dark") {
       document.body.classList.add("dark");
-      btnRef.current.classList.add('dark-mode-btn--active')
+      btnRef.current.classList.add("dark-mode-btn--active");
     } else {
       document.body.classList.remove("dark");
-      btnRef.current.classList.remove('dark-mode-btn--active')
+      btnRef.current.classList.remove("dark-mode-btn--active");
     }
   }, [darkMode]);
 
